c9cccdb10d602791d0604c293f9a61eaa19e40a7
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-05-17 19:23:09 +0300

    initial commit

---
+++ fabelcommon/datetime/time_formats.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+
+
+class TimeFormats():
+    def __init__(self):
+        pass
+
+    def get_date_time(self):
+        timestamp = datetime.now()
+        formatted_timestamp = timestamp.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+        return formatted_timestamp
+
+    def get_time_delta_hours(self, delta_hours):
+        yesterday = datetime.now() - timedelta(hours=delta_hours)
+        yesterday_formatted = yesterday.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+        return yesterday_formatted
+
+    def format_date_time_string(self, date_time):
+        date_time_obj = datetime.strptime(date_time[:-6], '%Y-%m-%dT%H:%M:%S.%f')
+        datetime_string = date_time_obj.strftime("%d.%m.%Y")
+        return datetime_string
+
+    def format_date_time_string_utc(self, date_time):
+        date_time_obj = datetime.strptime(date_time[:-6], '%Y-%m-%dT%H:%M:%S.%f')
+        utc_string = date_time_obj.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
+        return utc_string
+
+    def convert_to_minutes(self, duration):
+        duration_split = duration.split(':')
+        hours = duration_split[0]
+        minutes = duration_split[1]
+        if hours == '00':
+            hours = '0'
+
+        total_duration = int(hours) * 60 + int(minutes)
+        return total_duration
