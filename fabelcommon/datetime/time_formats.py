59c7c0f5687fea7e87251f8e17616c0a6595f3d6
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-05-20 14:22:16 +0300

    mark static methods

--- fabelcommon/datetime/time_formats.py
+++ fabelcommon/datetime/time_formats.py
@@ -2,33 +2,36 @@ from datetime import datetime, timedelta
 import pytz
 
 
-class TimeFormats():
-    def __init__(self):
-        pass
+class TimeFormats:
 
-    def get_date_time(self):
+    @staticmethod
+    def get_date_time():
         time_gmt = datetime.utcnow()
         date_str = time_gmt.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
         mytime = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ")
         mytime = mytime.replace(tzinfo=pytz.timezone('GMT'))
         return mytime.strftime("%a, %d %b %Y %H:%M:%S %Z")
 
-    def get_time_delta_hours(self, delta_hours):
+    @staticmethod
+    def get_time_delta_hours(delta_hours):
         yesterday = datetime.now() - timedelta(hours=delta_hours)
         yesterday_formatted = yesterday.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
         return yesterday_formatted
 
-    def format_date_time_string(self, date_time):
+    @staticmethod
+    def format_date_time_string(date_time):
         date_time_obj = datetime.strptime(date_time[:-6], '%Y-%m-%dT%H:%M:%S.%f')
         datetime_string = date_time_obj.strftime("%d.%m.%Y")
         return datetime_string
 
-    def format_date_time_string_utc(self, date_time):
+    @staticmethod
+    def format_date_time_string_utc(date_time):
         date_time_obj = datetime.strptime(date_time[:-6], '%Y-%m-%dT%H:%M:%S.%f')
         utc_string = date_time_obj.strftime('%Y-%m-%dT%H:%M:%S') + 'Z'
         return utc_string
 
-    def convert_to_minutes(self, duration):
+    @staticmethod
+    def convert_to_minutes(duration):
         duration_split = duration.split(':')
         hours = duration_split[0]
         minutes = duration_split[1]
