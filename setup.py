9f04e8dd482b3bc075de40958aaa3718e5faf2b5
Karlis Fersters <karlfer@statoilfuelretail.com>
2022-05-26 17:31:03 +0300

    packages added manually to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ setup(
     name='fabelcommon',
     version='1.1.0',
     description='Common API and functions',
-    packages=['fabelcommon', 'fabelcommon/bokbasen', 'fabelcommon/feed', 'fabelcommon/beat', 'fabelcommon/datetime'],
+    packages=[
+        'fabelcommon',
+        'fabelcommon/bokbasen',
+        'fabelcommon/feed',
+        'fabelcommon/beat',
+        'fabelcommon/datetime',
+        'fabelcommon/json',
+        'fabelcommon/beat/releases'
+    ],
     install_requires=['djangorestframework', 'pytz', 'requests', 'xmltodict'],
 )
