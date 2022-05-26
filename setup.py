530b54724e79a5334709088a44f1b91d5a4bb201
Karlis Fersters <karlfer@statoilfuelretail.com>
2022-05-26 09:45:17 +0300

    fabelcommon version 1.0->1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='fabelcommon',
-    version='1.0',
+    version='1.1.0',
     description='Common API and functions',
     packages=['fabelcommon', 'fabelcommon/bokbasen', 'fabelcommon/feed', 'fabelcommon/beat', 'fabelcommon/datetime'],
     install_requires=['djangorestframework', 'pytz', 'requests', 'xmltodict'],
