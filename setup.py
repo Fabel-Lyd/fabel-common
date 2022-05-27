34d206b4770f1a28dce406bc5c478c9de07899c8
Karlis Fersters <karlfer@statoilfuelretail.com>
2022-05-27 11:31:44 +0300

    result translation added to ReleasesService.search()

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='fabelcommon',
-    version='1.1.0',
+    version='1.2.0',
     description='Common API and functions',
     packages=[
         'fabelcommon',
