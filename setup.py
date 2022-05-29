53bca923b1ae36a2de65e6a786fedb6d00a6506c
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-05-29 19:40:47 +0300

    add setuptools for package discovery

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='fabelcommon',
     version='1.2.0',
     description='Common API and functions',
-    packages=[
-        'fabelcommon',
-        'fabelcommon/bokbasen',
-        'fabelcommon/feed',
-        'fabelcommon/beat',
-        'fabelcommon/datetime',
-        'fabelcommon/json',
-        'fabelcommon/beat/releases',
-        'fabelcommon/bokbasen/export',
-        'fabelcommon/feed/export'
-    ],
+    packages=find_packages(),
     install_requires=['djangorestframework', 'pytz', 'requests', 'xmltodict'],
 )
