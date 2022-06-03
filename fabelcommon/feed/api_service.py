aa98e60c7dc14b31a7e9cc02ef6e291d09dc6e5c
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-06-03 13:33:41 +0300

    add feed api service class

---
+++ fabelcommon/feed/api_service.py
@@ -0,0 +1,51 @@
+from typing import Dict
+import requests
+from requests import Response
+from fabelcommon.http.verbs import HttpVerb
+
+
+class FeedApiService:
+    BASE_URL: str = 'https://lydbokforlaget-feed.isysnet.no'
+    PRODUCT_EXPORT: str = '/export/export'
+
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        self._client_id: str = client_id
+        self._client_secret: str = client_secret
+
+    @staticmethod
+    def __create_headers(access_token: str) -> Dict[str, str]:
+        headers: Dict[str, str] = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
+        return headers
+
+    def __get_token(self) -> str:
+        url: str = f'{self.BASE_URL}/token-server/oauth/token'
+        data: Dict[str, str] = {
+            'grant_type': 'client_credentials',
+        }
+
+        response: Response = requests.post(
+            url,
+            data=data,
+            verify=True,
+            allow_redirects=False,
+            auth=(self._client_id, self._client_secret)
+        )
+        response.raise_for_status()
+
+        return response.json()['access_token']
+
+    @staticmethod
+    def build_url(base_url, action, parameters):
+        return f'{base_url}{action}?{parameters}'
+
+    def send_request(self, verb: HttpVerb, url, data=None):
+        token: str = self.__get_token()
+        headers: Dict[str, str] = self.__create_headers(token)
+
+        response: Response = requests.request(verb.value, url, headers=headers, data=data)
+        response.raise_for_status()
+
+        return response.json()['content']
---
+++ fabelcommon/http/verbs.py
@@ -0,0 +1,6 @@
+import enum
+
+
+class HttpVerb(enum.Enum):
+    GET = 'GET'
+    POST = 'POST'
