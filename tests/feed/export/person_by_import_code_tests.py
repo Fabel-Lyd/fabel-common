df5994042200cbdae9950ce6d76199579aeac9f8
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-06-03 14:38:59 +0300

    add feed export class

---
+++ tests/feed/export/person_by_import_code_tests.py
@@ -0,0 +1,37 @@
+from fabelcommon.feed.api_service import FeedApiService
+from fabelcommon.feed.export.export import FeedExport
+from fabelcommon.json.json_files import read_json_data
+
+
+def test_person_by_import_code_successful(mocker):
+    test_data = read_json_data('tests/feed/export/data/person_by_import_code_two.json')
+
+    mocker.patch.object(
+        FeedApiService,
+        attribute='send_request',
+        side_effect=test_data['response_data']
+    )
+
+    feed_api_service = FeedApiService('fake_client_id', 'fake_client_secret')
+
+    feed_export = FeedExport(feed_api_service)
+    persons_found = feed_export.person_by_import_code(['99991', '99992'])
+
+    assert persons_found == test_data['expected']
+
+
+def test_person_by_import_code_not_found(mocker):
+    test_data = read_json_data('tests/feed/export/data/person_not_found.json')
+
+    mocker.patch.object(
+        FeedApiService,
+        attribute='send_request',
+        side_effect=test_data['response_data']
+    )
+
+    feed_api_service = FeedApiService('fake_client_id', 'fake_client_secret')
+
+    feed_export = FeedExport(feed_api_service)
+    persons_found = feed_export.person_by_import_code(['99991'])
+
+    assert persons_found == test_data['expected']
