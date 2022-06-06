a85a645f489994d27fc54865e234d753e4f52302
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-06-06 15:44:15 +0300

    refactor feed export tests to reduce duplication

--- tests/feed/export/products_by_import_code_tests.py
+++ tests/feed/export/products_by_import_code_tests.py
@@ -1,12 +1,25 @@
 from typing import Dict, List
+import pytest
 from fabelcommon.feed.api_service import FeedApiService
 from fabelcommon.feed.export.export import FeedExport
 from fabelcommon.json.json_files import read_json_data
 from fabelcommon.feed.export.export import ProductType
 
 
-def test_persons_by_import_code_successful(mocker) -> None:
-    test_data: Dict = read_json_data('tests/feed/export/data/persons_by_import_code_two.json')
+@pytest.mark.parametrize(
+    'product_type, search_parameters, data_file_name',
+    [
+        (ProductType.PERSON, ['99991', '99992'], 'tests/feed/export/data/persons_by_import_code.json'),
+        (ProductType.PERSON, ['99991'], 'tests/feed/export/data/persons_not_found.json'),
+    ])
+def test_products_by_import_code(
+        product_type: ProductType,
+        search_parameters: List[str],
+        data_file_name: str,
+        mocker
+) -> None:
+
+    test_data: Dict = read_json_data(data_file_name)
 
     mocker.patch.object(
         FeedApiService,
@@ -17,23 +30,6 @@ def test_persons_by_import_code_successful(mocker) -> None:
     feed_api_service: FeedApiService = FeedApiService('fake_client_id', 'fake_client_secret')
 
     feed_export: FeedExport = FeedExport(feed_api_service)
-    persons_found: List[Dict] = feed_export.products_by_import_code(['99991', '99992'], ProductType.PERSON)
-
-    assert persons_found == test_data['expected']
-
-
-def test_persons_by_import_code_not_found(mocker) -> None:
-    test_data: Dict = read_json_data('tests/feed/export/data/persons_not_found.json')
-
-    mocker.patch.object(
-        FeedApiService,
-        attribute='send_request',
-        side_effect=test_data['response_data']
-    )
-
-    feed_api_service: FeedApiService = FeedApiService('fake_client_id', 'fake_client_secret')
-
-    feed_export = FeedExport(feed_api_service)
-    persons_found: List = feed_export.products_by_import_code(['99991'], ProductType.PERSON)
+    persons_found: List[Dict] = feed_export.products_by_import_code(search_parameters, product_type)
 
     assert persons_found == test_data['expected']
