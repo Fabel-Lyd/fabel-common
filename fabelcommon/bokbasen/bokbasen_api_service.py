fd06273a9f3f44e6445f0837b32da95f3737652b
Karlis Norins <karlis.norins@i-standards.com>
2022-06-07 11:43:50 +0300

    BokbasenApiService created

---
+++ fabelcommon/bokbasen/bokbasen_api_service.py
@@ -0,0 +1,47 @@
+from abc import ABC
+from typing import Dict
+import requests
+from requests import Response
+from fabelcommon.http.verbs import HttpVerb
+from fabelcommon.datetime.time_formats import TimeFormats
+
+
+class BokbasenApiService(ABC):
+    BASE_URL = 'https://api.boknett.no'
+
+    def __init__(self, username: str, password: str):
+        self._username = username
+        self._password = password
+
+    @staticmethod
+    def create_headers(ticket: str) -> Dict[str, str]:
+
+        headers = {
+            "Authorization": f"Boknett {ticket}",
+            "Date": TimeFormats.get_date_time(),
+            "Accept": "application/json"
+        }
+        return headers
+
+    def get_ticket(self):
+
+        url = 'https://login.boknett.no/v1/tickets'
+
+        params = {
+            "username": self._username,
+            "password": self._password
+        }
+
+        response = requests.post(url, params)
+        response.raise_for_status()
+
+        return response.headers['boknett-TGT']
+
+    def send_request(self, verb: HttpVerb, url: str, data=None) -> str:
+        token: str = self.get_ticket()
+        headers: Dict[str, str] = self.create_headers(token)
+
+        response: Response = requests.request(verb.value, url, headers=headers, data=data)
+        response.raise_for_status()
+
+        return response.text
