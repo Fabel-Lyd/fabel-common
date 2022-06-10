0fc4e13d5b1f8a1fe65484bd4971b71948513703
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-06-10 17:09:15 +0300

    refactor feed export

--- fabelcommon/feed/export/export.py
+++ fabelcommon/feed/export/export.py
@@ -1,14 +1,15 @@
 from typing import List, Dict
+from requests import Response
 from fabelcommon.feed.api_service import FeedApiService
 from fabelcommon.feed.export.product_types import ProductType
 from fabelcommon.http.verbs import HttpVerb
 
 
-class FeedExport:
-    feed_api_service: FeedApiService
+class FeedExport(FeedApiService):
+    PRODUCT_EXPORT: str = '/export/export'
 
-    def __init__(self, feed_api_service: FeedApiService) -> None:
-        self.feed_api_service: FeedApiService = feed_api_service
+    def __init__(self, client_id: str, client_secret: str) -> None:
+        super().__init__(client_id, client_secret)
 
     def products_by_name(
             self,
@@ -20,12 +21,12 @@ class FeedExport:
         result: List[Dict] = []
 
         for name in names:
-            url: str = FeedApiService.build_url(
-                FeedApiService.BASE_URL,
-                FeedApiService.PRODUCT_EXPORT,
-                f'changesOnly=false&productTypeImportCodes={product_type.value}&name={name}')
+            url: str = self.__build_url(
+                f'changesOnly=false&productTypeImportCodes={product_type.value}&name={name}'
+            )
 
-            response: List[Dict] = self.feed_api_service.send_request(HttpVerb.POST, url)
+            response: List[Dict] = self.__send_request(url)
+
             if product_type == ProductType.PERSON and len(response) > 1:
                 raise Exception(f'Multiple persons named "{name}" found in Feed')
 
@@ -35,10 +36,15 @@ class FeedExport:
     def products_by_import_code(self, import_codes: List[str], product_type: ProductType) -> List[Dict]:
         concatenated_import_codes: str = ','.join(import_codes)
 
-        url: str = FeedApiService.build_url(
-            FeedApiService.BASE_URL,
-            FeedApiService.PRODUCT_EXPORT,
-            f'changesOnly=false&productTypeImportCodes={product_type.value}&importCodes={concatenated_import_codes}&size=500&page=0')
+        url: str = self.__build_url(
+            f'changesOnly=false&productTypeImportCodes={product_type.value}&importCodes={concatenated_import_codes}&size=500&page=0'
+        )
 
-        response: List[Dict] = self.feed_api_service.send_request(HttpVerb.POST, url)
-        return response
+        return self.__send_request(url)
+
+    def __build_url(self, parameters: str) -> str:
+        return f'{self.BASE_URL}{self.PRODUCT_EXPORT}?{parameters}'
+
+    def __send_request(self, url: str) -> List[Dict]:
+        response: Response = self._send_request(HttpVerb.POST, url)
+        return response.json()['content']
