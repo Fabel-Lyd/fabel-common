f7b43416d24d97e9df0e82ea485f12cc6f4c6cad
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-06-17 14:23:00 +0300

    update tests

--- tests/feed/export_service/get_products_by_name_tests.py
+++ tests/feed/export_service/get_products_by_name_tests.py
@@ -5,23 +5,28 @@ from fabelcommon.feed.export_service import FeedExport
 from fabelcommon.feed.export_service import ProductType
 
 
+TEST_DATA_DIRECTORY: str = 'tests/feed/export_service/data/get_products_by_name'
+
+
 @pytest.mark.parametrize(
-    'product_type, search_parameters, data_file_name',
+    'data_file_name, product_type, search_parameters',
     [
-        (ProductType.PERSON, ['a', 'b'], 'tests/feed/export_service/data/get_products_by_name/successful_found.json'),
-        (ProductType.PERSON, ['a'], 'tests/feed/export_service/data/get_products_by_name/successful_not_found.json'),
-        (ProductType.BOOK, ['a', 'b'], 'tests/feed/export_service/data/get_products_by_name/successful_books_with_same_name.json'),
+        ('successful_found.json', ProductType.PERSON, ['a', 'b']),
+        ('successful_not_found.json', ProductType.PERSON, ['a']),
+        ('successful_books_with_same_name.json', ProductType.BOOK, ['a', 'b']),
     ])
-def test_get_products_by_name(
+def test_get_products_by_name_successful(
+        data_file_name: str,
         product_type: ProductType,
         search_parameters: List[str],
-        data_file_name: str,
         mocker
 ) -> None:
 
-    test_data: Dict = read_json_data(data_file_name)
+    test_data: Dict = read_json_data(f'{TEST_DATA_DIRECTORY}/{data_file_name}')
 
-    mocker.patch.object(
+    expected_endpoint_partial: str = f'https://lydbokforlaget-feed.isysnet.no/export/export?changesOnly=false&productTypeImportCodes={product_type.value}&name='
+
+    test_request = mocker.patch.object(
         FeedExport,
         attribute='_FeedExport__send_request',
         side_effect=test_data['response_data']
@@ -30,11 +35,15 @@ def test_get_products_by_name(
     feed_export = FeedExport('fake_client_id', 'fake_client_secret')
     products_found: List[Dict] = feed_export.get_products_by_name(search_parameters, product_type)
 
+    expected_endpoints: List[str] = [expected_endpoint_partial + name for name in search_parameters]
+    actual_endpoints: List[str] = [call.args[0] for call in test_request.call_args_list]
+
+    assert expected_endpoints == actual_endpoints
     assert products_found == test_data['expected']
 
 
-def test_get_products_by_name_duplicate_person(mocker) -> None:
-    test_data: Dict = read_json_data('tests/feed/export_service/data/get_products_by_name/failed_duplicate_person.json')
+def test_get_products_by_name_failed(mocker) -> None:
+    test_data: Dict = read_json_data(f'{TEST_DATA_DIRECTORY}/failed_duplicate_person.json')
 
     mocker.patch.object(
         FeedExport,
