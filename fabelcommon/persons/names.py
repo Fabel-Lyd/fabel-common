810ae213e06adf6ada0d0ada2b1e57deb948cf03
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-06-28 11:56:32 +0300

    add separate fix_inverted_name functions for string and list

--- fabelcommon/persons/names.py
+++ fabelcommon/persons/names.py
@@ -1,15 +1,14 @@
 from typing import List
 
 
+def fix_inverted_name(name: str) -> str:
+    if ',' not in name:
+        return name
+
+    name_parts: List[str] = name.split(sep=', ', maxsplit=1)
+    name_parts.reverse()
+    return ' '.join(name_parts)
+
+
 def fix_inverted_names(names: List[str]) -> List[str]:
-    fixed_name_list: List[str] = []
-    for name in names:
-        if ',' not in name:
-            fixed_name_list.append(name)
-            continue
-
-        name_parts: List[str] = name.split(sep=', ', maxsplit=1)
-        name_parts.reverse()
-        fixed_name_list.append(' '.join(name_parts))
-
-    return fixed_name_list
+    return [fix_inverted_name(name) for name in names]
