873235b4d7ec9181b23e801df53d249a9abc1eb0
Niko Dukalskis <nikola.dukalska@i-standards.com>
2022-07-04 16:15:12 +0300

    fix type hints, change read_xml_etree return value

--- fabelcommon/xmls/xml.py
+++ fabelcommon/xmls/xml.py
@@ -1,5 +1,6 @@
 from xml.etree.ElementTree import XMLParser
 from lxml import etree
+from lxml.etree import _Element, _ElementTree
 
 
 def read_xml_str(xml_file_name: str) -> str:
@@ -7,13 +8,13 @@ def read_xml_str(xml_file_name: str) -> str:
         return xml_file.read()
 
 
-def read_xml_etree(xml_file_name: str) -> etree:
-    tree = etree.parse(xml_file_name)
-    return tree
+def read_xml_etree(xml_file_name: str) -> _Element:
+    tree: _ElementTree = etree.parse(xml_file_name)
+    return tree.getroot()
 
 
-def xml_to_etree(xml_str: str) -> etree:
+def xml_to_etree(xml_str: str) -> _Element:
     utf8_parser: XMLParser = etree.XMLParser(encoding='utf-8')
     xml_str_as_utf8: bytes = xml_str.encode('utf-8')
-    tree: etree = etree.fromstring(xml_str_as_utf8, parser=utf8_parser)
-    return tree
+    tree_root: _Element = etree.fromstring(xml_str_as_utf8, parser=utf8_parser)
+    return tree_root
