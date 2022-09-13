284616759dbdac6bdc84f1f8de00d55ae49abd02
Karlis Fersters <karlis@i-standards.com>
2022-09-13 17:03:26 +0300

    refactor Beat API service

--- tests/beat/beat_service_tests.py
+++ tests/beat/beat_service_tests.py
@@ -3,7 +3,6 @@ import pytest
 from requests import HTTPError
 from rest_framework import status
 from fabelcommon.beat.beat_api_service import BeatApiService
-
 from fabelcommon.http.verbs import HttpVerb
 
 
@@ -39,20 +38,29 @@ def test_send_request_handle_error(mocker, requests_mock):
     with pytest.raises(HTTPError):
         beat_api_service.send_request(
             verb=HttpVerb.GET,
-            url='http://failing-endpoint',
-            data={})
+            url='http://failing-endpoint')
+
 
 def test_send_request_success(mocker, requests_mock):
 
     mocker.patch.object(BeatApiService, attribute='get_token', return_value='test_token')
-    requests_mock.get(
+    mocked_beat_request = requests_mock.post(
         'http://beat-endpoint',
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        text='{"releases":[]}',
+        request_headers={
+            'range': 'releases=0-6',
+            'Authorization': 'Bearer test_token',
+            'Content-Type': 'application/json'
+        })
 
     beat_api_service = BeatApiService('test_client_id', 'test_client_secret')
 
-    with pytest.raises(HTTPError):
-        beat_api_service.send_request(
-            verb=HttpVerb.GET,
-            url='http://failing-endpoint',
-            data={})
+    response_data = beat_api_service.send_request(
+        verb=HttpVerb.POST,
+        url='http://beat-endpoint',
+        data={'test_data': 'test_value'},
+        headers_to_add={'range': 'releases=0-6'}
+    )
+
+    assert json.loads(response_data) == {'releases': []}
+    assert mocked_beat_request.last_request.text == '{"test_data": "test_value"}'
