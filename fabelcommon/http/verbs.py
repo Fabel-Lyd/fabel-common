db5b9acee3560b33c7dfb529d24c86f50d6f0853
Karlis Fersters <karlis@i-standards.com>
2022-09-13 13:40:31 +0300

    add all HTTP verbs

--- fabelcommon/http/verbs.py
+++ fabelcommon/http/verbs.py
@@ -4,3 +4,10 @@ import enum
 class HttpVerb(enum.Enum):
     GET = 'GET'
     POST = 'POST'
+    PATCH = 'PATCH'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+    CONNECT = 'CONNECT'
+    HEAD = 'HEAD'
+    OPTIONS = 'OPTIONS'
+    TRACE = 'TRACE'
