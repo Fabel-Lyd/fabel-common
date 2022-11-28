d6c3b0597aee522ef7759b9552d0120c4f41bfc6
Karlis Norins <karlis.norins@i-standards.com>
2022-11-28 09:27:52 +0200

    refactor beat_delivery_service.py and add tests beat_delivery_service_tests.py

--- fabelcommon/beat/beat_delivery_api_service.py
+++ fabelcommon/beat/beat_delivery_api_service.py
@@ -1,13 +1,42 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 from requests import Response
+from fabelcommon.access_token_key import AccessTokenKey
 from fabelcommon.api_service import ApiService
 from fabelcommon.http.verbs import HttpVerb
+
+HARDCODED_DELIVERY_BASE_URL = 'https://beat-delivery'
 
 
 class BeatDeliveryApiService(ApiService):
 
-    def __init__(self, username, password, base_url: str, auth_path: str):
-        super().__init__(username, password, base_url, auth_path)
+    def __init__(
+            self,
+            username: str,
+            password: str,
+            base_url: str = HARDCODED_DELIVERY_BASE_URL,
+            auth_path: str = '/oauth'
+    ):
+        super().__init__(
+            client_id=username,
+            client_secret=password,
+            base_url=base_url,
+            auth_path=auth_path,
+        )
+
+    @property
+    def _access_token_key(self) -> AccessTokenKey:
+        return AccessTokenKey.ACCESS_TOKEN
+
+    @property
+    def _token_request_data(self) -> Dict:
+        return {}
+
+    @property
+    def _token_request_auth(self) -> Optional[Any]:
+        return None
+
+    def create_header(self, access_token: str) -> Dict:
+        return {'Authorization': f'Bearer {access_token}'}
 
     def send_request(
             self,
@@ -17,7 +46,7 @@ class BeatDeliveryApiService(ApiService):
             data: Optional[Dict] = None
     ) -> str:
         response: Response = self._send_request(
-            verb,
+            verb=verb,
             path=path,
             data=data,
             files=files)
