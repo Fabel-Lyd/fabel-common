a27924f483bfba96fba308036246ab2e5787041f
Karlis Fersters <karlis@i-standards.com>
2023-02-14 18:31:38 +0200

    add user_id to AccessToken

--- fabelcommon/access_token.py
+++ fabelcommon/access_token.py
@@ -1,9 +1,16 @@
 from datetime import datetime, timedelta
+from typing import Optional
 
 
 class AccessToken:
-    def __init__(self, access_token_value: str, expires_in: int):
+    def __init__(
+            self,
+            access_token_value: str,
+            expires_in: int,
+            user_id: Optional[str]
+    ):
         self.__access_token_value = access_token_value
+        self.__user_id = user_id
 
         current_time = datetime.now()
         self.__expires_at = current_time + timedelta(seconds=expires_in)
@@ -11,6 +18,10 @@ class AccessToken:
     @property
     def access_token_value(self) -> str:
         return self.__access_token_value
+
+    @property
+    def user_id(self) -> Optional[str]:
+        return self.__user_id
 
     @property
     def is_valid(self) -> bool:
--- tests/access_token_tests.py
+++ tests/access_token_tests.py
@@ -12,6 +12,8 @@ from fabelcommon.access_token import AccessToken
 def test_expired_access_token(expires_in: int, is_valid: bool):
     access_token = AccessToken(
         access_token_value='fake-token',
-        expires_in=expires_in)
+        expires_in=expires_in,
+        user_id=None
+    )
 
     assert access_token.is_valid is is_valid
