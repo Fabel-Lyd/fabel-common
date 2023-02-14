ec868c09b5e553bb2875c25d180a68f4a8a60bcc
Karlis Fersters <karlis@i-standards.com>
2023-02-14 18:43:57 +0200

    use freeze_time in test_expired_access_token()

--- tests/access_token_tests.py
+++ tests/access_token_tests.py
@@ -1,19 +1,23 @@
 import pytest
+from freezegun import freeze_time
 from fabelcommon.access_token import AccessToken
+
+freeze_time("2012-01-14 12:00:00").start()
+access_token = AccessToken(
+    access_token_value='fake-token',
+    expires_in=300,
+    user_id=None
+)
 
 
 @pytest.mark.parametrize(
-    'expires_in, is_valid',
+    'current_time, is_valid',
     [
-        (600, True),
-        (0, False),
-        (-600, False)
+        ("2012-01-14 12:00:01", True),
+        ("2012-01-14 12:04:59", True),
+        ("2012-01-14 12:05:00", False),
+        # ("2012-01-14 11:00:00", False)
     ])
-def test_expired_access_token(expires_in: int, is_valid: bool):
-    access_token = AccessToken(
-        access_token_value='fake-token',
-        expires_in=expires_in,
-        user_id=None
-    )
-
+def test_expired_access_token(current_time: str, is_valid: bool):
+    freeze_time(current_time).start()
     assert access_token.is_valid is is_valid
