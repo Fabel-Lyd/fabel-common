94dd3b89cf5e09845ff88e0bdf5542e14329926f
Karlis Fersters <karlis@i-standards.com>
2023-02-14 15:28:34 +0200

    create AccessToken

---
+++ tests/access_token_tests.py
@@ -0,0 +1,17 @@
+import pytest
+from fabelcommon.access_token import AccessToken
+
+
+@pytest.mark.parametrize(
+    'expires_in, is_valid',
+    [
+        (600, True),
+        (0, False),
+        (-600, False)
+    ])
+def test_expired_access_token(expires_in: int, is_valid: bool):
+    access_token = AccessToken(
+        access_token_value='fake-token',
+        expires_in=expires_in)
+
+    assert access_token.is_valid is is_valid
