cae302fd13e04d93419e7c539c8f016319a7758f
Karlis Fersters <karlis@i-standards.com>
2023-02-14 15:28:34 +0200

    create AccessToken

---
+++ fabelcommon/access_token.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+
+
+class AccessToken:
+    def __init__(self, access_token_value: str, expires_in: int):
+        self.__access_token_value = access_token_value
+
+        current_time = datetime.now()
+        self.__expires_at = current_time + timedelta(seconds=expires_in)
+
+    @property
+    def access_token_(self) -> str:
+        return self.__access_token_value
+
+    @property
+    def is_valid(self) -> bool:
+        return self.__expires_at > datetime.now()
