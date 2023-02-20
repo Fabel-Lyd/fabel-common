f5263ea38cd8cdc7c56220359bbdff83b9fc2a79
Karlis Fersters <karlis@i-standards.com>
2023-02-20 13:52:20 +0200

    include py.typed in setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     version='10.0.0',
     description='Common API and functions',
     packages=find_packages(),
+    package_data={'': ['py.typed']},
     install_requires=['djangorestframework', 'pytz', 'requests', 'xmltodict'],
 )
