82d2d5b8717ecfde1872f8906c90e3c037eac88f
Karlis Norins <karlis.norins@i-standards.com>
2023-03-02 13:30:20 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='fabelcommon',
-    version='11.0.0',
+    version='12.0.0',
     description='Common API and functions',
     packages=find_packages(),
     package_data={'': ['py.typed']},
