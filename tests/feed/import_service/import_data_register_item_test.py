87a569988050adf9715d687959bf5fe50783d48a
Karlis Norins <karlis.norins@i-standards.com>
2023-06-05 12:41:33 +0300

    fix type hints

--- tests/feed/import_service/import_data_register_item_test.py
+++ tests/feed/import_service/import_data_register_item_test.py
@@ -18,25 +18,6 @@ PAYLOAD_DATA: Dict = {
 }
 
 
-def test_import_data_register_item_success(requests_mock):
-
-    requests_mock.post(
-        'https://lydbokforlaget-feed.isysnet.no/token-server/oauth/token',
-        text=json.dumps({'access_token': 'test_token'})
-    )
-
-    requests_mock.patch(
-        'https://lydbokforlaget-feed.isysnet.no/import/basedata/dataRegisters/test-produsert-av',
-        status_code=status.HTTP_200_OK,
-        text='[]'
-    )
-
-    feed_import: FeedImport = FeedImport('test_username', 'test_password')
-    import_register_item = feed_import.import_data_register_items('test-produsert-av', PAYLOAD_DATA)
-
-    assert import_register_item is None
-
-
 def test_import_data_register_item_fail(requests_mock):
 
     requests_mock.post(
