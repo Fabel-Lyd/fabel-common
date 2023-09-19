fab41b17ac6bd6ddbaf05fb4e44aed3dd5c38d65
KarlisFersters <karlis@i-standards.com>
2023-09-19 10:54:44 +0300

    bump version to 21.3.0

--- version.py
+++ version.py
@@ -1,4 +1,9 @@
-PACKAGE_VERSION: str = '21.2.5'
+# MAJOR version when you make incompatible API changes
+# MINOR version when you add functionality in a backward compatible manner
+# PATCH version when you make backward compatible bug fixes
+# add suffix for release candidates, for example, 21.3.0-rc2
+
+PACKAGE_VERSION: str = '21.3.0'
 
 
 if __name__ == "__main__":
