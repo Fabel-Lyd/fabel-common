a13784265d5aebe6ec451b38a4c60ffa11797157
Niko Dukalskis <nikola.dukalska@i-standards.com>
2024-03-07 11:17:32 +0200

    change get_distinct_list to preserve original order

--- fabelcommon/list/list.py
+++ fabelcommon/list/list.py
@@ -2,4 +2,10 @@ from typing import List, Any
 
 
 def get_distinct_list(input_list: List[Any]) -> List[Any]:
-    return sorted(list(set(input_list)))
+    distinct_items: List[Any] = []
+
+    for item in input_list:
+        if item not in distinct_items:
+            distinct_items.append(item)
+
+    return distinct_items
--- tests/list/list_tests.py
+++ tests/list/list_tests.py
@@ -16,13 +16,13 @@ from fabelcommon.list.list import get_distinct_list
         ),
         (
             [2, 1, 1, 4, 5, 5],
-            [1, 2, 4, 5]
+            [2, 1, 4, 5]
         ),
         (
             ['b', 'a', 'a', 'd', 'e', 'e'],
-            ['a', 'b', 'd', 'e']
+            ['b', 'a', 'd', 'e']
         )
     ]
 )
 def test_get_distinct_list(input_list: List[Any], expected_list: List[Any]) -> None:
-    assert sorted(get_distinct_list(input_list)) == expected_list
+    assert get_distinct_list(input_list) == expected_list
