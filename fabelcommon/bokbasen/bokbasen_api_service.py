fe3a21592d4d3e83a7fdf0e738bf181cf15e44c0
Karlis Norins <karlis.norins@i-standards.com>
2024-03-26 09:49:47 +0200

    replace `BokbasenMetadataApiService` with refactored `BokbasenApiService`

--- fabelcommon/bokbasen/bokbasen_api_service.py
+++ fabelcommon/bokbasen/bokbasen_api_service.py
@@ -1,65 +1,81 @@
-from abc import ABC
-from typing import Dict, Any, Optional
-from urllib.parse import urljoin
+from typing import Dict, Optional, Tuple, Any
 import requests
 from requests import Response
+from fabelcommon.access_token import AccessToken
+from fabelcommon.access_token_key import AccessTokenKey
+from fabelcommon.api_service import ApiService
+from fabelcommon.bokbasen.audiences.audience import BokbasenAudience
+from fabelcommon.bokbasen.export_response import BokbasenExportResponse
 from fabelcommon.bokbasen.export_response.download_response import DownloadResponse
 from fabelcommon.bokbasen.export_response.order_response import OrderResponse
-from fabelcommon.http.verbs import HttpVerb
 from fabelcommon.datetime.time_formats import TimeFormats
+from fabelcommon.http.verbs import HttpVerb
 from fabelcommon.utilities.response_extension import ResponseExtension
 
 
-class BokbasenApiService(ABC):
+class BokbasenApiService(ApiService):
+
+    @property
+    def _access_token_key(self) -> AccessTokenKey:
+        return AccessTokenKey.ACCESS_TOKEN
+
+    @property
+    def _token_request_auth(self) -> Optional[Tuple]:
+        return None
+
+    @property
+    def _token_request_data(self) -> Dict:
+        return {
+            'client_id': self._client_id,
+            'client_secret': self._client_secret,
+            'audience': f'{self._base_url}/{self._audience}/',
+            'grant_type': 'client_credentials'
+        }
 
     def __init__(
             self,
-            username: str,
-            password: str,
-            base_url: str = 'https://api.boknett.no',
-            auth_url: str = 'https://login.boknett.no/v1/tickets'
+            client_id: str,
+            client_secret: str,
+            base_url: str,
+            auth_path: str,
+            audience: BokbasenAudience
     ) -> None:
-
-        self._username: str = username
-        self._password: str = password
-        self._base_url: str = base_url
-        self._auth_url: str = auth_url
-
-    @staticmethod
-    def create_headers(ticket: str) -> Dict[str, str]:
-
-        headers: Dict[str, str] = {
-            "Authorization": f"Boknett {ticket}",
-            "Date": TimeFormats.get_date_time()
-        }
-        return headers
-
-    def get_ticket(self) -> str:
-
-        params: Dict[str, str] = {
-            "username": self._username,
-            "password": self._password
-        }
-
-        response: Response = requests.post(self._auth_url, params)
-        response.raise_for_status()
-
-        return response.headers['boknett-TGT']
-
-    def send_request(self, verb: HttpVerb, url: str, data: Any = None, headers_to_add: Optional[Dict[str, str]] = None) -> str:
-        response: Response = self.__send_request(
-            verb=verb,
-            url=url,
-            data=data,
-            headers_to_add=headers_to_add
+        super().__init__(
+            client_id=client_id,
+            client_secret=client_secret,
+            base_url=base_url,
+            auth_path=auth_path,
+            audience=audience.value
         )
 
+    def _create_authorization_header(self, access_token: str) -> Dict:
+        return {
+            'Authorization': f'Bearer {access_token}',
+            'Date': TimeFormats.get_date_time()
+        }
+
+    def send_request(
+            self,
+            verb: HttpVerb,
+            url: str,
+            data: Any = None,
+            headers_to_add: Optional[Dict[str, str]] = None
+    ) -> str:
+        access_token: AccessToken = self._get_token()
+
+        response: Response = self._send_request(
+            verb=verb,
+            path=url,
+            data=data,
+            token_value=access_token.access_token_value,
+            headers_to_add=headers_to_add
+        )
         return response.text
 
     def send_order_request(self, verb: HttpVerb, url: str, data: Any = None) -> OrderResponse:
-        response: Response = self.__send_request(
+        response: Response = self._send_request(
             verb=verb,
-            url=url,
+            path=url,
             data=data,
             headers_to_add={
                 "Accept": "application/json",
@@ -69,46 +85,38 @@ class BokbasenApiService(ABC):
         return OrderResponse(location=response.headers['Location'])
 
     def send_download_url_request(self, url: str) -> DownloadResponse:
-
         params: Dict[str, str] = {
             "type": "audio/vnd.bokbasen.complete-public",
             "bitrate": "64"
         }
-
-        response: Response = self.__send_request(
+        response: Response = self._send_request(
             verb=HttpVerb.GET,
-            url=url,
+            path=url,
             data=None,
             params=params,
             headers_to_add={"Accept": "application/json"},
-            allow_redirect=False
-        )
+            allow_redirects=False)
 
         return DownloadResponse(response.headers['Location'])
 
-    def __send_request(
-            self,
-            verb: HttpVerb,
-            url: str,
-            data: Any,
-            params: Optional[Dict] = None,
-            headers_to_add: Optional[Dict[str, str]] = None,
-            allow_redirect: bool = True
-    ) -> Response:
-        token: str = self.get_ticket()
-        headers: Dict[str, str] = self.create_headers(token)
-
-        if headers_to_add is not None:
-            headers.update(headers_to_add)
-
-        response: Response = requests.request(
-            method=verb.value,
-            url=urljoin(self._base_url, url),
-            headers=headers,
-            data=data,
-            params=params,
-            allow_redirects=allow_redirect
+    def __create_token(self, token_request_data: Dict) -> AccessToken:
+        response: Response = requests.post(
+            url=self._auth_path,
+            data=token_request_data,
+            verify=True,
+            allow_redirects=False
         )
         ResponseExtension.raise_for_error(response=response)
 
-        return response
+        token_data: Dict = response.json()
+
+        access_token: AccessToken = AccessToken(
+            access_token_value=token_data[self._access_token_key.value],
+            expires_in=token_data.get('expires_in', token_data['expires_in']),
+            user_id=None
+        )
+        return access_token
+
+    def send_export_request(self, url: str) -> BokbasenExportResponse:
+        response: Response = self._send_request(HttpVerb.GET, url, None)
+        return BokbasenExportResponse(content=response.text, cursor=response.headers['next'])
