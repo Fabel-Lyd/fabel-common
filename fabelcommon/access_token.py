a8af615d6e39e91f68d999723ade555ad77ea492
Karlis Norins <karlis.norins@i-standards.com>
2024-05-27 15:33:44 +0300

    add refresh_token to AccessToken

--- fabelcommon/access_token.py
+++ fabelcommon/access_token.py
@@ -7,13 +7,15 @@ class AccessToken:
             self,
             access_token_value: str,
             expires_in: int,
-            user_id: Optional[str]
+            user_id: Optional[str],
+            refresh_token_value: Optional[str]
     ):
         self.__access_token_value = access_token_value
         self.__user_id = user_id
 
         current_time = datetime.now()
         self.__expires_at = current_time + timedelta(seconds=expires_in)
+        self.__refresh_token_value = refresh_token_value
 
     @property
     def access_token_value(self) -> str:
@@ -22,6 +24,10 @@ class AccessToken:
     @property
     def user_id(self) -> Optional[str]:
         return self.__user_id
+
+    @property
+    def refresh_token_value(self) -> Optional[str]:
+        return self.__refresh_token_value
 
     @property
     def is_valid(self) -> bool:
