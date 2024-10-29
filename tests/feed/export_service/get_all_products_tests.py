d6be997479a30596c018dfc9f7301f440ad317da
Karlis Norins <karlis.norins@i-standards.com>
2024-10-29 09:35:13 +0200

    add `get_all_products_pageable()`
    wip
    
    update version to `23.6.0-rc1`
    
    add `get_all_products_pageable()`
    
    wip

--- tests/feed/export_service/get_all_products_tests.py
+++ tests/feed/export_service/get_all_products_tests.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Generator
 import pytest
 import json
 from datetime import datetime
@@ -41,3 +41,33 @@ def test_get_all_products(export_from: Optional[str], requests_mock) -> None:
     actual_result: List[Dict] = feed_export.get_all_products(ProductType.PERSON, datetime(year=2024, month=1, day=12, hour=20, minute=0, second=0, tzinfo=pytz.UTC) if export_from else None)
 
     assert expected_result == actual_result
+
+
+def test_get_all_products_pageable(requests_mock) -> None:
+
+    requests_mock.post(
+        'https://lydbokforlaget-feed.isysnet.no/token-server/oauth/token',
+        text=json.dumps({'access_token': 'test_token'})
+    )
+
+    for page in range(3):
+        requests_mock.post(
+            f'https://lydbokforlaget-feed.isysnet.no/export/export?'
+            f'changesOnly=false&'
+            f'productTypeImportCodes=person'
+            f'&size={PAGE_SIZE}'
+            f'&page={page}',
+            text=read_json_as_text(f'{TEST_DATA_DIRECTORY}/exported_product_{page + 1}.json')
+        )
+
+    feed_export: FeedExport = FeedExport('test_username', 'test_password')
+
+    generator: Generator[List[Dict], None, None] = feed_export.get_all_products_pageable(
+        product_type=ProductType.PERSON
+    )
+
+    result: List[Dict] = []
+    for items in generator:
+        result.extend(items)
+
+    assert len(result) == 2
